ba08272f0662622ba216bc04c431d6b0e7d79da2
Maša Mićović <micovic.ra54.2020@uns.ac.rs>
2024-05-24 16:18:13 +0100

    feat: added user login and logout

--- circle-backend/controllers/userController.js
+++ circle-backend/controllers/userController.js
@@ -24,7 +24,7 @@ const signupUser = async(req, res) => {
 
         if(newUser) {
             generateTokenAndSetCookie(newUser._id, res);
-            
+
             res.status(201).json({
                 _id: newUser._id,
                 name: newUser.name,
@@ -40,6 +40,40 @@ const signupUser = async(req, res) => {
         res.status(500).json({message: err.message})
         console.log("Error in signupUser: ", err.message)
     }
+};
+
+const loginUser = async(req, res) => {
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if(!user || !isPasswordCorrect) return res.status(400).json({message: "Invalid username or password"});
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            userneme: user.username
+        });
+    }
+    catch(error) {
+        res.status(500).json({message: error.message});
+        console.log("Error in loginUser: ", error.message);
+    }
+};
+
+const logoutUser = async(req, res) => {
+    try {
+        res.cookie("jwt", "", {maxAge:1});
+        res.status(200).json({message: "User logged out successfully"});
+    }
+    catch(error) {
+        res.status(500).json({message: error.message});
+        console.log("Error in logoutUser: ", error.message)
+    }
 }
 
-export {signupUser}
+export {signupUser, loginUser, logoutUser}
--- circle-backend/routes/userRoutes.js
+++ circle-backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { signupUser } from "../controllers/userController.js";
+import { signupUser, loginUser, logoutUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 
 export default router;
