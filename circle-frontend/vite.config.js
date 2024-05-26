8589ea40540ae529e52688501404cc85a5d76d9f
Kshitij <kshitij.mukh@gmail.com>
2024-05-26 19:23:54 +0200

    feat: added profile update page

--- circle-frontend/vite.config.js
+++ circle-frontend/vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    //Get rid of the CORS errors
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://localhost:6000",
         changeOrigin: true,
         secure: false
       }
