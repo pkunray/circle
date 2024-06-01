158136cfa61dae32d317a02c1aac6a7608d23adb
pkunray <pkunray@gmail.com>
2024-06-01 13:01:18 +0200

    infra: Socket io client setup

---
+++ circle-frontend/src/context/SocketContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import io from "socket.io-client";
+import userAtom from "../atoms/userAtom";
+
+const SocketContext = createContext();
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketContextProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const user = useRecoilValue(userAtom);
+
+  useEffect(() => {
+    const socket = io("http://localhost:6000", {
+      query: {
+        userId: user?._id,
+      },
+    });
+    setSocket(socket);
+    return () => socket && socket.close();
+  }, [user?._id]);
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
+};
