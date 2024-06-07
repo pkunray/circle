cffd02ae795a176ef4dd1198446966e4422c39f2
pkunray <pkunray@gmail.com>
2024-06-07 18:41:50 +0200

    bugfix: Fix incorrect local storage item name

--- circle-frontend/src/hooks/useLogout.js
+++ circle-frontend/src/hooks/useLogout.js
@@ -3,32 +3,32 @@ import { useSetRecoilState } from "recoil";
 import useShowToast from "./useShowToast";
 
 const useLogout = () => {
-	const setUser = useSetRecoilState(userAtom);
-	const showToast = useShowToast();
+  const setUser = useSetRecoilState(userAtom);
+  const showToast = useShowToast();
 
-	const logout = async () => {
-		try {
-			const res = await fetch("/api/users/logout", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
-			const data = await res.json();
+  const logout = async () => {
+    try {
+      const res = await fetch("/api/users/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
 
-			if (data.error) {
-				showToast("Error", data.error, "error");
-				return;
-			}
+      if (data.error) {
+        showToast("Error", data.error, "error");
+        return;
+      }
 
-			localStorage.removeItem("user-threads");
-			setUser(null);
-		} catch (error) {
-			showToast("Error", error, "error");
-		}
-	};
+      localStorage.removeItem("user-circle");
+      setUser(null);
+    } catch (error) {
+      showToast("Error", error, "error");
+    }
+  };
 
-	return logout;
+  return logout;
 };
 
 export default useLogout;
